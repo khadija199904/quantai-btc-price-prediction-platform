8688fe3070dc086e150a98359af72bd6f364fb6f
Maryemelb <maryemelbergui@gmail.com>
2026-01-20 10:19:25 +0100

    save bronze data

--- spark/utils/spark_session.py
+++ spark/utils/spark_session.py
@@ -1,3 +1,11 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("btc_price_prediction").getOrder()
+spark = SparkSession.builder \
+    .appName("btc_price_prediction") \
+    .config(
+        "spark.jars.packages",
+        "org.postgresql:postgresql:42.7.3"
+    ) \
+    .getOrCreate()
+
+
