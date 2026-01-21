b5c9202232488ac7e12f14f7de926378f6d0d4c3
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-01-21 17:02:46 +0100

    Setup la confuguration de la database

--- api/core/config.py
+++ api/core/config.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 
 
-# Load environment variables from .env
+
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -10,16 +10,15 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL :
     # Fetch variables
-     USER = os.getenv("user")
-     PASSWORD = os.getenv("password")
-     HOST = os.getenv("host")
-     PORT = os.getenv("port")
-     DBNAME = os.getenv("dbname")
+     USER = os.getenv("POSTGRES_USER")
+     PASSWORD = os.getenv("POSTGRES_PASSWORD")
+     HOST = os.getenv("POSTGRES_HOST")
+     PORT = os.getenv("POSTGRES_PORT")
+     DBNAME = os.getenv("POSTGRES_DB")
 
 
      # Construct the SQLAlchemy connection string
      DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
-
 
  # Configuration de JWT
 SECRET_KEY = os.getenv("SECRET_KEY")
