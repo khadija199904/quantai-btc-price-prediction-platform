828fcbfd7b8762a2f53cf22325eea7c74c7a2321
Khadija199904 <khadijaelabbioui1999@gmail.com>
2026-01-21 17:01:06 +0100

    Ajout get_current_user dans dependencies.py

--- api/dependencies.py
+++ api/dependencies.py
@@ -1,4 +1,10 @@
-from .database import SessionLocal
+from api.database import SessionLocal
+from fastapi import Depends,Header,HTTPException
+from sqlalchemy.orm import Session
+from api.core.config import SECRET_KEY 
+from api.models.users import USERS
+from jose import JWTError, jwt
+
 
 
 # Dépendance pour la base de données
@@ -9,3 +15,25 @@ def get_db():
     finally:
         db.close()
 
+# verificatin de token crée en login
+def get_current_user (db: Session = Depends(get_db), token : str = Header()):
+    if token is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Token d'authentification manquant dans le header"
+        )
+    try :
+       payload = jwt.decode(token,key=SECRET_KEY,algorithms="HS256")
+       user_id = payload.get("id")
+       if user_id is None:
+           raise HTTPException(status_code=403, detail="Token invalide : ID utilisateur absent")
+       
+    except JWTError:
+      raise HTTPException(status_code=401,detail="Token expiré ou corrompu")
+
+    user_db = db.query(USERS).filter(USERS.id == user_id).first()
+    
+    
+    if not user_db:
+        raise HTTPException(status_code=404, detail="Utilisateur non trouvé")
+    return user_db
